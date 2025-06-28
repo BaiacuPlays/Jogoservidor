92ffad90d160c9b3d669b751a134237115ef34c4
BaiacuPlays <andreibonatto8@gmail.com>
2025-06-28 15:17:41 -0300

    foi?

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d4092bc9ea325703"; }
+/******/ 		__webpack_require__.h = function() { return "449dee2b7df52080"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.1ae96b1790a05ab2.hot-update.js
+++ .next/static/webpack/webpack.1ae96b1790a05ab2.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d4092bc9ea325703"; }
+/******/ 	__webpack_require__.h = function() { return "449dee2b7df52080"; }
 /******/ }();
 /******/ 
 /******/ }
